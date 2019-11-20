2832b4bee2a02d9fe7a18556af5ce19f4a2bb57c
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-20 18:46:34 -0200

    Creating hotelReservation class and making up the test structure

---
+++ src/test/java/Test/ExpediaBooking.java
@@ -0,0 +1,39 @@
+package Test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExpediaBooking {
+	
+	@Test
+	public void hotelReservation() {
+		
+		//1. Search
+		
+		
+		//2. Modify the search results page, give criteria
+		
+		
+		//3. Analize the result and make our selection
+		
+		
+		//4. Booking reservation
+		
+		
+		//5. Fill our contact / billing
+		
+		
+		//6. Get confirmation
+	}
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+}
