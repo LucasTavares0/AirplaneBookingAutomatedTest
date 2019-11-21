d9d026008b007acbbefb18654800be174788a80f
LucasTavares0 <lucas.tavares@modalgr.com.br>
2019-11-20 22:40:19 -0200

    Update enhanncing test steps and implementing switch window functionality

--- src/test/java/Pages/ExpediaHotelSearchpage.java
+++ src/test/java/Pages/ExpediaHotelSearchpage.java
@@ -4,19 +4,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ExpediaHotelSearchpage {
 	
 	WebDriver driver;
 	
 	//WebElements declaration
-	@FindBy(id = "star-3")
+	@FindBy(xpath = "//*[@id=\"star-3\"]")
 	WebElement FourStarCriteriaButton;
 	
+	@FindBy(css = "button[type='button'][data-stid='hotels-destination-dialog-trigger']")
+	WebElement DestinationTitleLabel;
+	
+	@FindBy(xpath = "//*[@id=\"app\"]/div/div/div/div/div[1]/main/div/div/div[2]/section[1]/ol/li[3]/div/div/a")
+	WebElement ThirdResult;
 	
 	//Action Methods
 	public void selectFourStarCriteria() {
 		FourStarCriteriaButton.click();
+	}
+	public String getTextFromDestinationTitle() {
+		return DestinationTitleLabel.getText();
+	}
+	public void clickOnThirdResult() {
+		ThirdResult.click();
 	}
 	
 	//Contructor
--- src/test/java/Test/ExpediaBooking.java
+++ src/test/java/Test/ExpediaBooking.java
@@ -1,11 +1,14 @@
 package Test;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Pages.ExpediaHomePage;
 import Pages.ExpediaHotelSearchpage;
@@ -16,17 +19,19 @@ public class ExpediaBooking {
 	public String childAge = "0";
 	public String checkInDate = "11/22/2019";
 	public String checkOutDate = "11/23/2019";
-	public String hotelDestinationCity = "New York, New York";
+	public String hotelDestinationCity = "Washington, District of Columbia";
 	public String browserType = "";
 	
 	WebDriver driver;
 	DriverFactory factory = new DriverFactory();
+	ArrayList<String> windows = new ArrayList<String>();
 	
 	@Test
 	public void hotelReservation() {
 		
 		//1. Search
 		ExpediaHomePage homepage = new ExpediaHomePage(driver);
+		System.out.println("WORKING IN PAGE = " + driver.getTitle());
 		
 		homepage.clickOnHotelButton();
 		homepage.clearDestinationTextBox(); 
@@ -43,11 +48,18 @@ public class ExpediaBooking {
 		
 		//2. Modify the search results page, give criteria
 		ExpediaHotelSearchpage hotelpage = new ExpediaHotelSearchpage(driver);
+		System.out.println("WORKING IN PAGE = " + driver.getTitle());
 		
-		hotelpage.selectFourStarCriteria();
+		System.out.println("DESTINATION CITY = " + hotelpage.getTextFromDestinationTitle());
+//		hotelpage.selectFourStarCriteria();		
 		
 		//3. Analize the result and make our selection
+		hotelpage.clickOnThirdResult();
 		
+		// Switch driver to new browser window
+		ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(windows.get(1));
+		System.out.println("WORKING IN PAGE = " + driver.getTitle());
 		
 		//4. Booking reservation
 		
@@ -60,13 +72,17 @@ public class ExpediaBooking {
 	
 	@Before
 	public void setUp() {
+		System.out.println("STARTING TEST AND OPENNING BROWSER...");
+		
 		driver = factory.openBrowser(browserType);
 		driver.get("http://www.expedia.com/");
-		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);	
 	}
 	
 	@After
-	public void tearDown() {		
+	public void tearDown() {
+		System.out.println("FINISHING TEST AND CLOSING BROWSER...");
+		
 //   	driver.quit();
 	}
 }
